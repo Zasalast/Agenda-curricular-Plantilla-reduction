98b7dac7e8044eec21fa2499810defd7a50b90a0
zasalast <zasalas60@gmail.com>
2019-12-06 20:53:38 -0500

    edit activity complete

--- src/components/Agenda/EditActivity.js
+++ src/components/Agenda/EditActivity.js
@@ -13,23 +13,31 @@ import Todos, { propTypes as TodosPropTypes } from 'components/Todos';
 import Typography from '../Typography';
 
 class EditActivity extends Component {
-
-     state = {
-          // activity: null,
-          curriculumactivity: '',
-          manager: '',
-          hour: '',
-          date: '',
-          description: '',
-          prioritySelected: null,
-          stateSelected: null,
-     }
+     constructor(props) {
+          super(props);
+          this.state = {
+               // activity: null,
+               curriculumactivity: '',
+               manager: '',
+               hour: '',
+               date: '',
+               description: '',
+               prioritySelected: null,
+               stateSelected: null,
+               // textprueba:"hola mundo"
+          }
+      
+          this.handleChange = this.handleChange.bind(this);
+         
+        }
+     
 
 
      componentDidMount() {
           const { data } = this.props.location.state;
           console.log(data);
 const{ curriculumactivity,
+     id,
      manager,
      hour,
      date,
@@ -37,15 +45,9 @@ const{ curriculumactivity,
      priority,
      state}=data;
 
-
-// curriculumactivity: data.curriculumactivity,
-//                manager: data.manager,
-//                hour: data.hour,
-//                date: data.date,
-//                description: data.description,
-//                prioritySelected: data.priority,
-//                stateSelected: data.state, 
           this.setState({    
+               activity:data,
+               id,
                curriculumactivity,
                manager,
                hour,
@@ -60,18 +62,19 @@ const{ curriculumactivity,
      createActivity = () => {
           const { date, description, hour,   manager, priority, state, curriculumactivity } = this.state;
           const {  id  } = this.state.activity;
+          
           if (date.trim() === '' || description.trim() === '' || hour.trim() === '' || manager.trim() === '' || priority === null || state === null || curriculumactivity.trim() === '') {
                toast.error("Todos los campos son necesarios")
           } else {
                const activityUpdate = {
-                    id:"6",
-                    curriculumactivity: "Muerte metodos",
-                    manager: "Adios elio",
-                    hour: "4:30",
-                    date: "2012-03-04",
-                    description: "Actualizando",
-                    priority: "Altaa",
-                    state: "1",
+                    id:id.toString(),
+                    curriculumactivity,
+                    manager,
+                    hour,
+                    date,
+                    description,
+                    priority,
+                    state,
                }
                console.log("activityUpdate"); console.log(activityUpdate);
                // enviar por props o petición de axios
@@ -111,6 +114,9 @@ const{ curriculumactivity,
                { defaultValue: 'Bajo', label: 'Bajo' }
           ]
           const { date, description, hour, id, manager, priority, state, curriculumactivity } = this.state.activity;
+          const stateSelected=state;
+          const prioritySelected=priority;
+
           return (
                <div className="cr-content">
                   
@@ -118,28 +124,28 @@ const{ curriculumactivity,
                          <div className="position-relative">
                               <div   >
                               <div className="form-group">
-                            <input onChange={this.onChangeInput} type="text" name="curriculumactivity" value= {id}   className="form-control" />
+                            <input  type="text" name="curriculumactivity" value= {id}   className="form-control" />
                                   
                               </div>
                                  
                                    <div className="form-group">                                  <label>Titulo Actividad:</label>
-                                        <input onChange={this.onChangeInput} type="text" name="curriculumactivity" defaultValue={curriculumactivity} className="form-control" />
+                                        <input onChange={this.handleChange} type="text" name="curriculumactivity" defaultValue={curriculumactivity} className="form-control" />
                                    </div>
                                    <div className="form-group">
                                         <label>Encargado:</label>
-                                        <input onChange={this.onChangeInput} type="text" name="manager" value={manager}     className="form-control" />
+                                        <input onChange={this.handleChange} type="text" name="manager" defaultValue={manager}     className="form-control" />
                                    </div>
                                    <div className="form-group">
                                         <label>Fecha :</label>
-                                        <input onChange={this.onChangeInput} type="text" name="date" defaultValue={date} className="form-control" />
+                                        <input onChange={this.handleChange} type="text" name="date" defaultValue={date} className="form-control" />
                                    </div>
                                    <div className="form-group">
                                         <label> Hora:</label>
-                                        <input onChange={this.onChangeInput} type="text" name="hour" defaultValue={hour}  className="form-control" />
+                                        <input onChange={this.handleChange} type="text" name="hour" defaultValue={hour}  className="form-control" />
                                    </div>
                                    <div className="form-group">
                                         <label> Descripción:</label>
-                                        <input onChange={this.onChangeInput} type="text" name="description" defaultValue={description} className="form-control" />
+                                        <input onChange={this.handleChange} type="text" name="description" defaultValue={description} className="form-control" />
                                    </div>
                                    <div className="form-group">
                                         <label> Prioridad:</label>
@@ -147,12 +153,20 @@ const{ curriculumactivity,
                   
                                         {/* <input onChange={this.onChangeInput} type="text" name="priority" defaultValue={priority} className="form-control" /> */}
                                    </div>
+{/* 
+                                   <label>
+                                         Essay:
+                                         <textarea defaultValue={this.state.textprueba} onChange={this.handleChange} />
+                                    </label> */}
+
                                    <div className="form-group">
                                         <label> Estado:</label>
                                         {/* <input onChange={this.onChangeInput} type="text" name="state" defaultValue={state} className="form-control" /> */}
                                         <Select onChange={this.onChangeState} name="state" options={staties} defaultValue={this.state.state} />
 
                                    </div>
+
+                                   
 
 
                               </div>
@@ -180,6 +194,13 @@ const{ curriculumactivity,
           let value = e.target.defaultValue;
           this.setState({ [state]: value })
      }
+
+     handleChange(event) {
+          let state = event.target.name;
+          let value = event.target.value;
+          this.setState({ [state]: value })
+          // this.setState({textprueba: event.target.value});
+        }
 
      render() {
         
